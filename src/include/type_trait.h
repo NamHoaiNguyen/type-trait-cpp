4d093369b01b15973676d3d72ba22b1777bbce55
NamHoaiNguyen <nguyen.hoai.nam.3912@gmail.com>
2021-10-10 19:09:58 +0700

    update lib

--- src/include/type_trait.h
+++ src/include/type_trait.h
@@ -1,3 +1,5 @@
+/*Helper classes*/
+
 template<typename T, T v>
 struct integral_constant {
     using value_type = T;
@@ -6,11 +8,184 @@ struct integral_constant {
     constexpr operator value_type() const noexcept { return value; }
 };
 
-template<bool T>
-using bool_constant = integral_constant<bool, T>;
+template<bool B>
+using bool_constant = integral_constant<bool, B>;
 
-template<bool T>
-using true_type =  integral_constant<bool,  true>;
+using true_type = integral_constant<bool, true>;
 
-template<bool T>
 using false_type = integral_constant<bool, false>;
+
+/*Const-volatility specifiers*/
+namespace remove_const_volatile {
+    template<typename T>
+    struct remove_const {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_const<T const> {
+        using type = T;
+    };
+
+    template<typename T>
+    using remove_const_t = typename remove_const<T>::type;
+
+    template<typename T>
+    struct remove_volatile {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_volatile<T volatile> {
+        using type = T;
+    };
+
+    template<typename T>
+    using remove_volatile_t = typename remove_volatile<T>::type;
+
+    template<typename T>
+    struct remove_cv {
+
+    };
+
+    template<typename T>
+    struct remove_cv<T const> : public remove_const<T> {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_cv<T volatile> : public remove_volatile<T> {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_cv<T const volatile> {
+        using type = T;
+    };
+
+    template<typename T>
+    using remove_cv_t = typename remove_cv<T>::type;
+};
+
+/*Prmary type categories*/
+namespace type_categories{
+    // template<typename T>
+    // struct is_void_helper : public false_type {
+
+    // };
+
+    // template<>
+    // struct is_void_helper<void> : public true_type {
+
+    // };
+
+    // template<typename T>
+    // struct is_void : public is_void_helper<remove_cv<T>> {
+
+    // };
+
+    // template<typename T>
+    // struct is_void : is_same<void, remove_cv<T>> {
+
+    // };
+}
+
+/*Miscellaneous transformations*/
+namespace transformation {
+    template<bool B, typename T = void>
+    struct enable_if {
+        
+    };
+
+    template<typename T>
+    struct enable_if<true, T> {
+        using type = T;
+    };
+
+    template<bool B, class T = void>
+    using enable_if_t = typename enable_if<B, T>::type;
+}
+
+/*Type relationships*/
+namespace type_relationships {
+    template<typename T, typename U>
+    struct is_same : public false_type {
+
+    };
+
+    template<typename T>
+    struct is_same<T, T> : public true_type {
+
+    };
+
+    template<typename T, typename U>
+    inline constexpr bool is_same_v = is_same<T, U>::value;
+}
+
+
+/*const-volatility specifiers*/
+
+
+/*Reference*/
+namespace reference {
+    template<typename T>
+    struct remove_reference {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_reference<T&> {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_reference<T&&> {
+        using type = T;
+    };
+
+    template<typename T>
+    using remove_reference_t = typename remove_reference<T>::type;
+
+    template<typename T>
+    struct add_lvalue_referece {
+
+    };
+
+    // template<typename T>
+}
+
+/*Pointer*/
+namespace pointer {
+    template<typename T>
+    struct remove_pointer {
+
+    };
+
+    template<typename T>
+    struct remove_pointer<T*> {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_pointer<T* const> {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_pointer<T* volatile> {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_pointer<T* const volatile> {
+        using type = T;
+    };
+
+    // template<typename T>
+    // using remove_pointer_t = typename remove_pointer<T>::type;
+    template<typename T>
+    using remove_pointer_t = typename pointer::remove_pointer<T>::type;
+}
+
+// template<typename T>
+// inline constexpr bool remove_pointer_t = typename pointer::remove_pointer<T>::type;
--- src/test/test.cc
+++ src/test/test.cc
@@ -1,4 +1,4 @@
-#define BOOST_TEST_MODULE stl_algorithms_test
+#define BOOST_TEST_MODULE type_trait_test
 
 #include <boost/test/included/unit_test.hpp>
 #include "../include/type_trait.h"
@@ -10,3 +10,40 @@ BOOST_AUTO_TEST_CASE (test_integral_constant) {
        "2*2 != 4"
     );
 }
+
+BOOST_AUTO_TEST_CASE(test_cv_specfier) {
+    using namespace remove_const_volatile;
+    using namespace type_relationships;
+
+    BOOST_TEST(bool(is_same_v<remove_const_t<int const>, int>) == true);
+    BOOST_TEST(bool(is_same_v<remove_const_t<float const>, double>) == false);
+    BOOST_TEST(bool(is_same_v<remove_volatile_t<int volatile>, int>) == true);
+    BOOST_TEST(bool(is_same_v<remove_volatile_t<int volatile>, float>) == false);
+    BOOST_TEST(bool(is_same_v<remove_cv_t<int* const >, int*>) == true);
+    BOOST_TEST(bool(is_same_v<remove_cv_t<float* volatile>, float*>) == true);
+    BOOST_TEST(bool(is_same_v<remove_cv_t<double* const volatile>, double*>) == true);
+}
+
+BOOST_AUTO_TEST_CASE(test_type_relationship) {
+    using namespace type_relationships;
+    BOOST_TEST(bool(is_same_v<int, int>) == true);
+    BOOST_TEST(bool(is_same_v<int, float>) == false);
+}
+
+BOOST_AUTO_TEST_CASE(test_reference) {
+    using namespace reference;
+    using namespace type_relationships;
+
+    BOOST_TEST(bool(is_same_v<remove_reference_t<int&&>, int>) == true);
+    BOOST_TEST(bool(is_same_v<remove_reference_t<int&>, int>) == true);
+    BOOST_TEST(bool(is_same_v<remove_reference_t<int&>, float>) == false);
+}
+
+BOOST_AUTO_TEST_CASE(test_pointer) {
+    using namespace pointer;
+    using namespace type_relationships;
+
+    BOOST_TEST(bool(is_same_v<remove_pointer_t<int*>, int>) == true);
+    BOOST_TEST(bool(is_same_v<remove_pointer_t<int* const volatile>, int>) == true);
+    BOOST_TEST(bool(is_same_v<remove_pointer_t<int*>, float>) == false);
+} 
